96cb68218330332eb08e7efb187cfc098381711f
qe617 <2664481691@qq.com>
2024-05-16 01:26:57 +0800

    add

---
+++ 01-start/app.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+function add(a, b, c) {
+    console.log(typeof n1);
+    console.log(c);
+    return a + b;
+}
+var n1 = 5;
+var n2 = 2.8;
+var res = true;
+console.log(add(n1, n2, res));
